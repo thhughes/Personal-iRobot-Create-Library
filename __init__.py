1d85fce6a2fe02c3a0b5d5add8ded5695814a402
thhughes <troy.h.hughes@gmail.com>
2015-07-17 13:54:47 -0700

    Added an __init__ and setup file - long term hope is an installable python library.

---
+++ __init__.py
@@ -0,0 +1,1 @@
+__all__ = ['CreateBase','CRE82','SerialSocket','Tests','lispUtils','Create']
---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='Troy Hughes iRobot Create Library',
+      version='v1.1'
+      author_email='troy.h.hughes@gmail.com'
+      package_dir = {'':'..'},
+      packages=['Create'])
